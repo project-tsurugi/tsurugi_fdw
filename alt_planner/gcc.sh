b0a5c3b6aa93b508da92923081924f9d2f3e402a
Nozomi Yanagisawa <noz-yanagisawa@vf.jp.nec.com>
2019-09-04 17:49:13 +0900

    警告メッセージ対応

--- alt_planner/gcc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pg_ctl stop
-gcc -Wall -shared -o alt_planner.so -I/home/yanagisawa/postgresql-11.1/src/include -fPIC -g3 alt_planner.c
-cp alt_planner.so /home/yanagisawa/pgsql/lib/
-pg_ctl start
