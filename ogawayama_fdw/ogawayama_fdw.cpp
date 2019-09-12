eade1f7b2c57492a1f57978f260f74143905b4d9
Kohei Okada <k-okada@pb.jp.nec.com>
2019-09-12 14:00:16 +0900

    Fixed comment.

--- ogawayama_fdw/ogawayama_fdw.cpp
+++ ogawayama_fdw/ogawayama_fdw.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright 2019-2019 tsurugi project.
-<<<<<<< HEAD
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +15,6 @@
  *
  *	@file	ogawayama_fdw.cpp
  *	@brief 	Foreign Data Wrapper for Ogawayama.
-=======
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
->>>>>>> 9d5b16f474cab972798c5594cb8b222ea52dc2c1
  */
 #ifdef __cplusplus
 extern "C" {
