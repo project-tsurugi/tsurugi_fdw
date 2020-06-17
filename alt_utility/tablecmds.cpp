4f45b648c09e14e56f0b4e36ffb3f394a3e02e78
Hiromasa Yano <hyano@nec.com>
2020-06-17 08:53:41 +0000

    This code does not work and is not tested

--- alt_utility/tablecmds.cpp
+++ alt_utility/tablecmds.cpp
@@ -32,7 +32,23 @@ extern "C" {
 #endif
 
 #include <iostream>
+#include <string>
+
+#include "manager/metadata/datatypes.h"
+#include "manager/metadata/metadata.h"
 #include "tablecmds.h"
+
+using namespace manager::metadata;
+using namespace boost::property_tree;
+
+std::unique_ptr<Metadata> datatypes{new DataTypes("Tsurugi")};
+
+const int TSURUGI_TYPE_CHAR_ID = 13;
+const int TSURUGI_TYPE_VARCHAR_ID = 14;
+
+const int TSURUGI_DIRECTION_DEFAULT = 0
+const int TSURUGI_DIRECTION_ASC = 1;
+const int TSURUGI_DIRECTION_DESC = 2;
 
 /*
  *  @brief:
@@ -41,17 +57,212 @@ bool define_relation(CreateStmt *stmt)
 {
     Assert(stmt != nullptr);
 
-    char *create_stmt;
-    create_stmt = nodeToString(stmt);
+    char *create_stmt_cstr;
+    bool ret_val{false};
 
-    std::cout << "nodeToString: " << create_stmt << "\n" << std::endl;
-    pfree(create_stmt);
+    create_stmt_cstr = nodeToString(stmt);
 
-    bool success = true;
-    if (!success)
-    {
-        elog(ERROR, "define_relation() failed.");
+    std::cout << "nodeToString: " << create_stmt_cstr << "\n" << std::endl;
+    pfree(create_stmt_cstr);
+
+    if (datatypes->load() != ErrorCode::OK) {
+        std::cout << "DataTypes::load() error." << std::endl;
+        return ret_val;
     }
 
-    return success;
+    // check syntax supported or not by Tsurugi
+    if(ret_val = check_syntax_supported(stmt)){
+        elog(ERROR, "define_relation() failed.");
+        return ret_val;
+    }
+
+    // check type supported or not by Tsurugi
+    if(ret_val = check_type_supported(stmt)){
+        elog(ERROR, "define_relation() failed.");
+        return ret_val;
+    }
+
+    // send metadata to metadata manager
+    if(ret_val = store_metadata(stmt)){
+        elog(ERROR, "define_relation() failed.");
+        return ret_val;
+    }
+
+    ret_val = true;
+    return ret_val;
+}
+
+bool check_type_supported(CreateStmt *stmt){
+    bool supported{false};
+
+    supported = true;
+    return supported;
+}
+
+bool check_syntax_supported(CreateStmt *stmt){
+    bool supported{false};
+    supported = true;
+    return supported;
+}
+
+bool store_metadata(CreateStmt *stmt){
+
+    bool ret_val{false};
+
+    // default constraint expression
+    const std::string default_constraint_expr{"(undefined)"}
+
+    // root
+    boost::property_tree::ptree root;
+
+    // table
+    boost::property_tree::ptree new_table;
+
+    // tbale name
+    if (stmt.relation != NIL){
+        RangeVar *relation = (RangeVar *)stmt.relation)
+        char *relname = relation.relname;
+        if (relation.relname != NULL){
+            new_table.put(Tables::NAME, relname);
+        }
+    }else{
+        return ret_val;
+    }
+
+    List *table_elts = stmt->tableElts;
+    ListCell *table_elt;
+
+    // primaryKey
+    boost::property_tree::ptree primary_keys;
+
+    // columns
+    boost::property_tree::ptree columns;
+    uint64_t ordinal_position = 1;
+
+    foreach (table_elt, table_elts)
+    {
+
+        // column
+        boost::property_tree::ptree column;
+
+        // ordinalPosition
+        column.put<uint64_t>(Tables::Column::ORDINAL_POSITION, ordinal_position);
+
+        // column name
+        column.put(Tables::Column::NAME, colDef.colname);
+
+        ColumnDef *colDef = (ColumnDef *)lfirst(table_elt);
+        List *colDef_constraints = colDef.constrains;
+
+        bool nullable = true;
+        bool pkey = false;
+
+        if(colDef_constraints != NIL){
+
+            foreach (l, colDef_constraints){
+                Constraint *constraint = (Constraint *)lfirst(l);
+
+                if(constraint.contype == CONSTR_NOTNULL){
+                    // nullable
+                    nullable = false;
+                }else if(constraint.contype == CONSTR_PRIMARY){
+                    // primary key
+                    pkey = true;
+                }
+            }
+        }
+
+        // nullable
+        column.put<bool>(Tables::Column::NULLABLE, nullable);
+
+        // primary key and direction
+        if (pkey){
+            boost::property_tree::ptree primary_key;
+            primary_key.put<uint64_t>("", ordinal_position);
+            primary_keys.push_back(std::make_pair("", primary_key));
+
+            column.put<uint64_t>(Tables::Column::DIRECTION,TSURUGI_DIRECTION_ASC);
+        }else{
+            column.put<uint64_t>(Tables::Column::DIRECTION,TSURUGI_DEFAULT);
+        }
+
+        // get dataTypeId
+        ErrorCode err;
+        boost::property_tree::ptree datatype;
+
+        TypeName *colDef_type_name = colDef.typeName;
+
+        if(colDef_type_name != NIL){
+            List type_names = colDef_type_name.names;
+            ObjectIdType data_type_id = NULL;
+
+            foreach (l, type_names){
+                Value *type_name_value = (Value *)lfirst(l);
+                err = datatypes_->get(DataTypes::PG_DATA_TYPE_QUALIFIED_NAME, type_name_value, datatype);
+                if (err != ErrorCode::OK) {
+                    data_type_id = datatype.get<ObjectIdType>(DataTypes::ID);
+                    if (!data_type_id) {
+                        return ret_val;
+                    }else{
+                        // put dataTypeId
+                        column.put<ObjectIdType>(Tables::Column::DATA_TYPE_ID, data_type_id);
+                    }
+                    break;
+                }
+            }
+
+            List typmods = colDef_type_name.typmods;
+            int32 typmod = colDef_type_name.typmod;
+
+            if (type_mods != NIL){
+                foreach (l, typmods){
+                    if (IsA(tm, A_Const))
+		            {
+			            A_Const    *ac = (A_Const *) tm;
+
+		                if (IsA(&ac->val, Integer))
+		                {
+                            column.put<uint64_t>(Tables::Column::DATA_LENGTH, ac->val.val.ival);
+		                }else{
+                            return ret_val;
+                        }
+                    }else{
+                        return ret_val;
+                    }
+                }
+            else if (typmod != 0 ){
+                column.put<uint64_t>(Tables::Column::DATA_LENGTH, typmod);
+            }
+            if (data_type_id != NULL){
+                if(data_type_id == TSURUGI_TYPE_VARCHAR_ID){
+                    // varying
+                    column.put<bool>(Tables::Column::VARYING, true);
+                }else if(data_type_id == TSURUGI_TYPE_CHAR_ID){
+                    // varying
+                    column.put<bool>(Tables::Column::VARYING, false);
+                }
+            }
+
+        }
+
+        // default constraint expression
+        column.put(Tables::Column::DEFAULT, default_constraint_expr);
+
+        columns.push_back(std::make_pair("", column));
+        ordinal_position++;
+    }
+
+    new_table.add_child(Tables::PRIMARY_KEY_NODE, primary_keys);
+
+    new_table.add_child(Tables::COLUMNS_NODE, columns);
+
+    root.add_child(Tables::TABLES_NODE, new_table);
+
+    if (Tables::save(dbname, root) != ErrorCode::OK) {
+        elog(ERROR, "define_relation() failed.");
+        return ret_val;
+    }
+
+    ret_val = true;
+    return ret_val;
 }
