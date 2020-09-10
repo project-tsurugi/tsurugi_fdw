58ab86b87597f177f93eaabf48424e805e8f718c
Hiromasa Yano <hyano@nec.com>
2020-09-10 08:33:22 +0000

    Fix PKEY/NOT NULL test

--- sql/otable_of_constr.sql
+++ sql/otable_of_constr.sql
@@ -5,6 +5,7 @@ CREATE DATABASE test TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_
 CREATE EXTENSION ogawayama_fdw;
 CREATE SERVER ogawayama FOREIGN DATA WRAPPER ogawayama_fdw;
 
+--error
 CREATE TABLE test1 (
 col1  int,
 col3  bigint,
@@ -23,6 +24,7 @@ col10 char(1000),
 col14 varchar(1000)
 ) tablespace tsurugi;
 
+--error
 CREATE TABLE test3 (
 col1  int NOT NULL,
 col3  bigint NOT NULL,
@@ -250,7 +252,7 @@ CREATE FOREIGN TABLE test4_2 (
 col1  int NOT NULL,
 col3  bigint NOT NULL,
 col5  real NOT NULL,
-col7  double precision NOT NULL,
+col7  double precision NOT NULL ,
 col10 char(1000) NOT NULL,
 col14 varchar(1000) NOT NULL
 ) SERVER ogawayama;
@@ -264,6 +266,25 @@ col10 char(1000),
 col14 varchar(1000)
 ) SERVER ogawayama;
 
+--error
+CREATE FOREIGN TABLE test6 (
+col0  integer,
+col1  int,
+col2  int4,
+col3  bigint,
+col4  int8,
+col5  real,
+col6  float4,
+col7  double precision,
+col8  float8,
+col9  char,
+col10 char(1000) ,
+col11 character,
+col12 character(1000),
+col14 varchar(1000),
+col16 character varying(1000)
+) SERVER ogawayama;
+
 CREATE FOREIGN TABLE test7 (
 col1  int NOT NULL,
 col3  bigint NOT NULL,
@@ -271,6 +292,44 @@ col5  real NOT NULL,
 col7  double precision NOT NULL,
 col10 char(1000) NOT NULL,
 col14 varchar(1000) NOT NULL
+) SERVER ogawayama;
+
+--error
+CREATE FOREIGN TABLE test8_1 (
+col0  integer NOT NULL,
+col1  int NOT NULL,
+col2  int4 NOT NULL,
+col3  bigint NOT NULL ,
+col4  int8 NOT NULL,
+col5  real NOT NULL,
+col6  float4 NOT NULL,
+col7  double precision NOT NULL,
+col8  float8 NOT NULL,
+col9  char NOT NULL,
+col10 char(1000) NOT NULL,
+col11 character NOT NULL,
+col12 character(1000) NOT NULL,
+col14 varchar(1000) NOT NULL,
+col16 character varying(1000) NOT NULL
+) SERVER ogawayama;
+
+--error
+CREATE FOREIGN TABLE test8_2 (
+col0  integer NOT NULL,
+col1  int NOT NULL,
+col2  int4 NOT NULL,
+col3  bigint  NOT NULL,
+col4  int8 NOT NULL,
+col5  real NOT NULL,
+col6  float4 NOT NULL,
+col7  double precision NOT NULL,
+col8  float8 NOT NULL,
+col9  char NOT NULL,
+col10 char(1000) NOT NULL,
+col11 character NOT NULL,
+col12 character(1000) NOT NULL,
+col14 varchar(1000) NOT NULL,
+col16 character varying(1000) NOT NULL
 ) SERVER ogawayama;
 
 CREATE FOREIGN TABLE test9 (
@@ -282,6 +341,25 @@ col10 char(1000),
 col14 varchar(1000)
 ) SERVER ogawayama;
 
+--error
+CREATE FOREIGN TABLE test10 (
+col0  integer,
+col1  int,
+col2  int4,
+col3  bigint ,
+col4  int8,
+col5  real,
+col6  float4,
+col7  double precision,
+col8  float8,
+col9  char,
+col10 char(1000),
+col11 character,
+col12 character(1000),
+col14 varchar(1000),
+col16 character varying(1000)
+) SERVER ogawayama;
+
 CREATE FOREIGN TABLE test11 (
 col1  int NOT NULL,
 col3  bigint NOT NULL,
@@ -291,82 +369,186 @@ col10 char(1000) NOT NULL,
 col14 varchar(1000) NOT NULL
 ) SERVER ogawayama;
 
+--error
+CREATE FOREIGN TABLE test12_1 (
+col0  integer NOT NULL,
+col1  int NOT NULL,
+col2  int4 NOT NULL,
+col3  bigint NOT NULL,
+col4  int8 NOT NULL,
+col5  real NOT NULL,
+col6  float4 NOT NULL,
+col7  double precision NOT NULL,
+col8  float8 NOT NULL,
+col9  char NOT NULL,
+col10 char(1000) NOT NULL,
+col11 character NOT NULL,
+col12 character(1000) NOT NULL,
+col14 varchar(1000) NOT NULL,
+col16 character varying(1000) NOT NULL
+) SERVER ogawayama;
+
+--error
+CREATE FOREIGN TABLE test12_2 (
+col0  integer NOT NULL,
+col1  int NOT NULL,
+col2  int4 NOT NULL,
+col3  bigint NOT NULL,
+col4  int8 NOT NULL,
+col5  real NOT NULL,
+col6  float4 NOT NULL,
+col7  double precision NOT NULL,
+col8  float8 NOT NULL,
+col9  char NOT NULL,
+col10 char(1000) NOT NULL,
+col11 character NOT NULL,
+col12 character(1000) NOT NULL,
+col14 varchar(1000) NOT NULL,
+col16 character varying(1000)
+) SERVER ogawayama;
+
+--test1 all error
 SELECT * from test1;
 INSERT INTO test1 VALUES (1,1000,3.24,-2.27,'A','ABCDEFABCDEF');
 SELECT * from test1;
 INSERT INTO test1 (col14) VALUES ('ABCDEFABCDEF');
 SELECT * from test1;
 
+--test2
 SELECT * from test2;
+--ok
 INSERT INTO test2 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test2;
-INSERT INTO test2 (col3) VALUES (1001);
+--PKEY col1 null error
+INSERT INTO test2 (col3,col5,col7,col10,col14) VALUES (1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test2;
+--PKEY col1 not unique error
 INSERT INTO test2 (col1) VALUES (1);
 SELECT * from test2;
+--ok
 INSERT INTO test2 (col1) VALUES (2);
 SELECT * from test2;
 
+--test3 all error
 INSERT INTO test3 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test3;
 INSERT INTO test3 (col3) VALUES (1);
 SELECT * from test3;
 
+--test4_1
+SELECT * from test4_1;
+--ok
 INSERT INTO test4_1 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test4_1;
+--NOT NULL constraint null error
+INSERT INTO test4_1 (col1) VALUES (3);
+SELECT * from test4_1;
+--PKEY col1 null error
 INSERT INTO test4_1 (col5) VALUES (3.24);
 SELECT * from test4_1;
+--PKEY col1 not unique error
 INSERT INTO test4_1 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test4_1;
+--ok
 INSERT INTO test4_1 VALUES (2,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test4_1;
 
+--test4_2
+SELECT * from test4_2;
+--ok
 INSERT INTO test4_2 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test4_2;
-INSERT INTO test4_2 (col7) VALUES (3.24);
+--PKEY col7 null error
+INSERT INTO test4_2 (col1,col3,col5,col10,col14) VALUES (1,1000,3.24,'ABC','ABCDEFABCDEF');
 SELECT * from test4_2;
+--NOT NULL constraint null error
+INSERT INTO test4_2 (col3,col5,col7,col10,col14) VALUES (1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
+SELECT * from test4_2;
+--PKEY col7 not unique error
 INSERT INTO test4_2 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test4_2;
+--ok
 INSERT INTO test4_2 VALUES (2,1000,3.24,-3.24,'ABC','ABCDEFABCDEF');
 SELECT * from test4_2;
 
+--test5
 SELECT * from test5;
+--ok
 INSERT INTO test5 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test5;
-INSERT INTO test5 (col14) VALUES ('ABCDEFABCDEF');
+--PKEY col10 null error
+INSERT INTO test5 (col1,col3,col5,col7,col14) VALUES (1,1000,3.24,-2.27,'ABCDEFABCDEF');
 SELECT * from test5;
-INSERT INTO test5 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
+--PKEY col10 not unique error
+INSERT INTO test5 (col10) VALUES ('ABC');
 SELECT * from test5;
+--ok
 INSERT INTO test5 (col10) VALUES ('AAA');
 SELECT * from test5;
 
+--test6 error
+SELECT * from test6;
+
+--test7
+SELECT * from test7;
+--ok
 INSERT INTO test7 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test7;
-INSERT INTO test7 (col14) VALUES ('ABC');
+-- NOT NULL constraint null error
+INSERT INTO test7 (col1,col3,col5,col7,col10) VALUES (1,1000,3.24,-2.27,'ABC');
 SELECT * from test7;
+--PKEY col3 null error
+INSERT INTO test7 (col1,col5,col7,col10,col14) VALUES (1,3.24,-2.27,'ABC','ABCDEFABCDEF');
+SELECT * from test7;
+--PKEY col3 not unique error
 INSERT INTO test7 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test7;
+--ok
 INSERT INTO test7 VALUES (1,1001,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test7;
 
+--test8 all error
+SELECT * from test8_1;
+SELECT * from test8_2;
+
+--test9
 SELECT * from test9;
+--ok
 INSERT INTO test9 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test9;
-INSERT INTO test9 (col7) VALUES (1,3.24,-2.27,'ABC');
+--PKEY (col1,col3,col5,col10) null error
+INSERT INTO test9 (col1,col3,col5) VALUES (2,1000,3.24);
 SELECT * from test9;
+--PKEY (col1,col3,col5,col10) not unique error
 INSERT INTO test9 (col1,col3,col5,col10) VALUES (1,1000,3.24,'ABC');
 SELECT * from test9;
+--ok
 INSERT INTO test9 (col1,col3,col5,col10) VALUES (1,1000,3.24,'A');
 SELECT * from test9;
 
+--test10 error
+SELECT * from test10;
+
+--test11
+SELECT * from test11;
+--ok
 INSERT INTO test11 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test11;
-INSERT INTO test11 (col7) VALUES (1.2);
+--NOT NULL constraint null error
+INSERT INTO test11 (col1,col3,col5,col10,col14) VALUES (3,1000,3.24,'ABC','ABCDEFABCDEF');
+--PKEY (col1,col3,col5,col10,col14) null error
+INSERT INTO test11 (col1,col5,col7,col10,col14) VALUES (1,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test11;
-INSERT INTO test11 (col1,col3,col5,col10,col14) VALUES (1,1000,3.24,'ABC','ABCDEFABCDEF');
+--PKEY (col1,col3,col5,col10,col14) not unique error
+INSERT INTO test11 VALUES (1,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test11;
-INSERT INTO test11 (col1,col3,col5,col10,col14) VALUES (2,1000,3.24,'ABC','ABCDEFABCDEF');
+--ok
+INSERT INTO test11 VALUES (2,1000,3.24,-2.27,'ABC','ABCDEFABCDEF');
 SELECT * from test11;
+
+--test12 error
+SELECT * from test12_1;
+SELECT * from test12_2;
 
 \c postgres
 
