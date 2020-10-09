af868dbcd5674b8d080bf45c3823073e147410dd
Hiromasa Yano <hyano@nec.com>
2020-10-09 06:19:27 +0000

    Add tests for foreign table with no column

--- sql/create_table_syntax_type.sql
+++ sql/create_table_syntax_type.sql
@@ -1,6 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS ogawayama_fdw;
 CREATE SERVER IF NOT EXISTS ogawayama FOREIGN DATA WRAPPER ogawayama_fdw;
 
+CREATE TABLE no_column (
+) tablespace tsurugi;
+
 CREATE TABLE customer_fifth (
   c_credit char(2) PRIMARY KEY
 ) tablespace tsurugi;
@@ -121,6 +124,9 @@ create table one_english_column_name (
   o int PRIMARY KEY
 ) tablespace tsurugi;
 
+CREATE FOREIGN TABLE no_column (
+) SERVER ogawayama;
+
 CREATE FOREIGN TABLE customer_fifth (
   c_credit char(2)
 ) SERVER ogawayama;
@@ -232,6 +238,10 @@ create foreign table japanese_column_name (
 create foreign table one_english_column_name (
   o int
 ) SERVER ogawayama;
+
+SELECT * FROM no_column;
+INSERT INTO no_column VALUES ();
+SELECT * FROM no_column;
 
 SELECT * FROM customer_fifth;
 INSERT INTO customer_fifth VALUES ('TE');
