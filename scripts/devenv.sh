5eeb1b196ba00c3249049e647d9fd8e9e78a445e
Yusuke Kitajima <yus-kitajima@nec.com>
2021-06-22 10:12:14 +0900

    Modified build scritps for ogawayama v3.0.

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -36,13 +36,18 @@ sudo apt install -y $(cat scripts/ubuntu.deps)
 mkdir -p $TSURUGI_HOME
 cd $TSURUGI_HOME
 git clone git@github.com:project-tsurugi/manager.git
-git clone git@github.com:project-tsurugi/masstree-beta.git
-git clone git@github.com:project-tsurugi/kvs_charkey.git
+#git clone git@github.com:project-tsurugi/masstree-beta.git
+#git clone git@github.com:project-tsurugi/kvs_charkey.git
 git clone git@github.com:project-tsurugi/shakujo.git
 git clone git@github.com:project-tsurugi/shirakami.git
 git clone git@github.com:project-tsurugi/sharksfin.git
-git clone git@github.com:project-tsurugi/umikongo.git
+#git clone git@github.com:project-tsurugi/umikongo.git
 git clone git@github.com:project-tsurugi/ogawayama.git
+git clone git@github.com:project-tsurugi/takatori.git
+git clone git@github.com:project-tsurugi/yugawara.git
+git clone git@github.com:project-tsurugi/jogasaki.git
+git clone git@github.com:project-tsurugi/mizugaki.git
+git clone git@github.com:project-tsurugi/sandbox-performance-tools.git
 
 # manager
 
@@ -63,8 +68,8 @@ make install
 
 # mastree
 
-cd $TSURUGI_HOME/masstree-beta
-git submodule update --init
+#cd $TSURUGI_HOME/masstree-beta
+#git submodule update --init
 
 #./bootstrap.sh
 #./configure
@@ -73,12 +78,12 @@ git submodule update --init
 
 # kvs_charkey
 
-cd $TSURUGI_HOME/kvs_charkey
-git submodule update --init
-
-rm -rf $BINARY_DIR
-mkdir $BINARY_DIR
-cd $BINARY_DIR
+#cd $TSURUGI_HOME/kvs_charkey
+#git submodule update --init
+#
+#rm -rf $BINARY_DIR
+#mkdir $BINARY_DIR
+#cd $BINARY_DIR
 
 #cmake -G Ninja \
 #    -DFORCE_INSTALL_RPATH=ON \
@@ -140,10 +145,84 @@ cmake -G Ninja \
 ninja
 ninja install
 
-# umikongo
+## umikongo
+#
+#cd $TSURUGI_HOME/umikongo
+#git submodule update --init third_party/googletest
+#
+#rm -rf $BINARY_DIR
+#mkdir $BINARY_DIR
+#cd $BINARY_DIR
+#cmake -G Ninja \
+#    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+#    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+#    -DFORCE_INSTALL_RPATH=ON \
+#    -DFIXED_PAYLOAD_SIZE=ON \
+#    -DBUILD_TESTS=OFF \
+#    -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION \
+#    ..
+#ninja
+#ninja install
 
-cd $TSURUGI_HOME/umikongo
-git submodule update --init third_party/googletest
+# takatori
+
+cd $TSURUGI_HOME/takatori
+git submodule update --init --recursive
+
+# fpdecimal(Sub module)
+cd $TSURUGI_HOME/takatori/third_party/fpdecimal
+rm -rf $BINARY_DIR
+mkdir $BINARY_DIR
+cd $BINARY_DIR
+
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DFORCE_INSTALL_RPATH=ON \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_DOCUMENTS=OFF \
+    ..
+#cmake --build . --target install --clean-first
+ninja
+ninja install
+
+cd $TSURUGI_HOME/takatori
+rm -rf $BINARY_DIR
+mkdir $BINARY_DIR
+cd $BINARY_DIR
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DFORCE_INSTALL_RPATH=ON \
+    -DBUILD_TESTS=OFF \
+    ..
+#cmake --build . --target install --clean-first
+ninja
+ninja install
+
+# yugawara
+
+cd $TSURUGI_HOME/yugawara
+git submodule update --init --recursive
+
+# hopscotch-map(Sub module)
+cd $TSURUGI_HOME/yugawara/third_party/hopscotch-map
+rm -rf $BINARY_DIR
+mkdir $BINARY_DIR
+cd $BINARY_DIR
+
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DFORCE_INSTALL_RPATH=ON \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_DOCUMENTS=OFF \
+    ..
+#cmake --build . --target install --clean-first
+ninja
+ninja install
+
+cd $TSURUGI_HOME/yugawara
 
 rm -rf $BINARY_DIR
 mkdir $BINARY_DIR
@@ -152,10 +231,75 @@ cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
     -DFORCE_INSTALL_RPATH=ON \
-    -DFIXED_PAYLOAD_SIZE=ON \
+    -DBUILD_TESTS=OFF \
+    ..
+#cmake --build . --target install --clean-first
+ninja
+ninja install
+
+# mizugaki
+cd $TSURUGI_HOME/
+
+rm -rf bison-3.5.1
+curl http://ftp.jaist.ac.jp/pub/GNU/bison/bison-3.5.1.tar.gz | tar zxv
+cd bison-3.5.1
+./configure --prefix=$INSTALL_PREFIX
+make -j4
+make install
+
+cd $TSURUGI_HOME/mizugaki
+
+git submodule update --init --recursive
+
+rm -rf $BINARY_DIR
+mkdir $BINARY_DIR
+cd $BINARY_DIR
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DFORCE_INSTALL_RPATH=ON \
+    -DBUILD_TESTS=OFF \
+    ..
+#cmake --build . --target install --clean-first
+ninja
+ninja install
+
+# sandbox-performance-tools
+
+cd $TSURUGI_HOME/sandbox-performance-tools
+git submodule update --init
+
+rm -rf $BINARY_DIR
+mkdir $BINARY_DIR
+cd $BINARY_DIR
+
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DFORCE_INSTALL_RPATH=ON \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_DOCUMENTS=OFF \
+    ..
+#cmake --build . --target install --clean-first
+ninja
+ninja install
+
+# jogasaki
+
+cd $TSURUGI_HOME/jogasaki
+git submodule update --init --recursive
+
+rm -rf $BINARY_DIR
+mkdir $BINARY_DIR
+cd $BINARY_DIR
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DFORCE_INSTALL_RPATH=ON \
     -DBUILD_TESTS=OFF \
     -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION \
     ..
+#cmake --build . --target install --clean-first
 ninja
 ninja install
 
