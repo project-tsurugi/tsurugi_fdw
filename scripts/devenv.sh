fb9cd80b4b96a5e590d9ef1229b97814271cf284
oniki <oniki.naokazu@kcc.co.jp>
2022-01-31 13:04:56 +0900

    Changed script to be function-based.

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -4,384 +4,324 @@
 
 set -eo pipefail
 
-TSURUGI_HOME=$HOME/project-tsurugi
-INSTALL_PREFIX=$HOME/.local
-SCRIPT_PATH=$(cd $(dirname $0); pwd)
+# Grobal variable
+{
+  TSURUGI_HOME=$HOME/project-tsurugi
+  INSTALL_PREFIX=$HOME/.local
+  SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
-#BUILD_TYPE=Release
-BUILD_TYPE=RelWithDebInfo
-BINARY_DIR=build
+  #BUILD_TYPE=Release
+  BUILD_TYPE=RelWithDebInfo
+  BINARY_DIR=build
 
-SHARKSFIN_IMPLEMENTATION=memory
+  SHARKSFIN_IMPLEMENTATION=memory
+}
 
-sudo apt update -y
-sudo apt install -y $(cat $SCRIPT_PATH/ubuntu.deps)
+package_install() {
+  echo -e "\nInstall the required packages for project."
 
-if [ ! -d $TSURUGI_HOME ]; then
-  mkdir -p $TSURUGI_HOME
-fi
-cd $TSURUGI_HOME
+  sudo apt update -y
+  sudo apt install -y $(cat $SCRIPT_PATH/ubuntu.deps)
 
-if [ ! -d $TSURUGI_HOME/manager ]; then
-  git clone git@github.com:project-tsurugi/manager.git
-fi
+  sudo apt autoremove -y
+}
 
-if [ ! -d $TSURUGI_HOME/shakujo ]; then
-  git clone git@github.com:project-tsurugi/shakujo.git
-fi
+get_source_file() {
+  local parent_dir=$(pwd)
+  local module="$1"
 
-if [ ! -d $TSURUGI_HOME/shirakami ]; then
-  git clone git@github.com:project-tsurugi/shirakami.git
-fi
+  if [[ -z $module ]]; then
+    get_source_file 'manager'
+    get_source_file 'shakujo'
+    get_source_file 'shirakami'
+    get_source_file 'sharksfin' third_party/googletest
+    get_source_file 'takatori' --recursive
+    get_source_file 'yugawara' --recursive
+    get_source_file 'mizugaki' --recursive
+    get_source_file 'sandbox-performance-tools'
+    get_source_file 'jogasaki' --recursive
+    get_source_file 'ogawayama'
+    return
+  fi
+  shift
 
-if [ ! -d $TSURUGI_HOME/sharksfin ]; then
-  git clone git@github.com:project-tsurugi/sharksfin.git
-fi
+  echo -e "\nGet the $module component from GitHub."
+  
+  if [[ -d $TSURUGI_HOME/$module ]]; then
+    # Get (pull) the component from GitHub.
+    cd $TSURUGI_HOME/$module
+    git pull
+  else
+    # Get (clone) the component from GitHub.
+    git clone git@github.com:project-tsurugi/$module.git $TSURUGI_HOME/$module
+    cd $TSURUGI_HOME/$module
+  fi
 
-if [ ! -d $TSURUGI_HOME/takatori ]; then
-  git clone git@github.com:project-tsurugi/takatori.git
-fi
+  # Update the submodule from GitHub.
+  git submodule update --init $*
 
-if [ ! -d $TSURUGI_HOME/yugawara ]; then
-  git clone git@github.com:project-tsurugi/yugawara.git
-fi
+  cd $parent_dir
+}
 
-#if [ ! -d $TSURUGI_HOME/tateyama ]; then
-#  git clone git@github.com:project-tsurugi/tateyama.git
-#fi
+build() {
+  local parent_dir=$(pwd)
+  local build_path=$TSURUGI_HOME/$1/$BINARY_DIR
 
-if [ ! -d $TSURUGI_HOME/jogasaki ]; then
-  git clone git@github.com:project-tsurugi/jogasaki.git
-fi
+  echo -e "\nBuild the $(basename $1) component."
 
-if [ ! -d $TSURUGI_HOME/mizugaki ]; then
-  git clone git@github.com:project-tsurugi/mizugaki.git
-fi
+  mkdir -p $build_path
+  cd $build_path
 
-if [ ! -d $TSURUGI_HOME/sandbox-performance-tools ]; then
-  git clone git@github.com:project-tsurugi/sandbox-performance-tools.git
-fi
+  cmake -G Ninja $* ..
 
-if [ ! -d $TSURUGI_HOME/ogawayama ]; then
-  git clone git@github.com:project-tsurugi/ogawayama.git
-fi
+  ninja | tee .ninja_tee
+  if [[ $(cat .ninja_tee) != 'ninja: no work to do.' ]]; then
+    ninja install
+  fi
+  rm -rf .ninja_tee > /dev/null 2>&1
 
-# manager
+  cd $parent_dir
+}
 
-echo -e "\nmanager started."
-cd $TSURUGI_HOME/manager
-git submodule update --init
+build_module() {
+  local parent_dir=$(pwd)
+  local module="$1"
 
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
+  if [[ -z $module ]]; then
+    build_module 'manager'
+    build_module 'shakujo'
+    build_module 'shirakami'
+    build_module 'sharksfin'
+    build_module 'takatori'
+    build_module 'yugawara'
+    build_module 'mizugaki'
+    build_module 'sandbox-performance-tools'
+    build_module 'jogasaki'
+    build_module 'ogawayama'
+    return
+  fi
 
-  cmake -G Ninja \
+  case "$module" in
+  # tsurugi::manager
+  'manager')
+    # Build the manager.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF
+    ;;
+
+  # tsurugi::shakujo
+  'shakujo')
+    # Build the shakujo.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
       -DFORCE_INSTALL_RPATH=ON \
       -DBUILD_TESTS=OFF \
       -DBUILD_DOCUMENTS=OFF \
-      ..
-  ninja
-  ninja install
-fi
+      -DBUILD_EXAMPLES=OFF
+    ;;
 
-# shakujo
-
-echo -e "\nshakujo started."
-cd $TSURUGI_HOME/shakujo
-git submodule update --init
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      -DFORCE_INSTALL_RPATH=ON \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_DOCUMENTS=OFF \
-      -DBUILD_EXAMPLES=OFF \
-      ..
-  ninja
-  ninja install
-fi
-
-# shirakami
-
-echo -e "\nshirakami started."
-cd $TSURUGI_HOME/shirakami/
-git submodule update --init 
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
+  # tsurugi::shirakami
+  'shirakami')
+    # Build the shirakami.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
       -DFORCE_INSTALL_RPATH=ON \
       -DBUILD_TESTS=ON \
-      -DBUILD_DOCUMENTS=OFF \
-      ..
-  ninja
-  ninja install
-fi
+      -DBUILD_DOCUMENTS=OFF
+#      -DBUILD_TESTS=OFF \
+    ;;
 
-# sharksfin
-
-echo -e "\nsharksfin started."
-cd $TSURUGI_HOME/sharksfin
-git submodule update --init third_party/googletest
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      -DFORCE_INSTALL_RPATH=ON \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_SHIRAKAMI=OFF \
-      -DBUILD_DOCUMENTS=OFF \
-      -DBUILD_EXAMPLES=OFF \
-      ..
-  ninja
-  ninja install
-fi
-
-# takatori
-
-cd $TSURUGI_HOME/takatori
-git submodule update --init --recursive
-
-# fpdecimal(Sub module)
-echo -e "\nfpdecimal started."
-cd $TSURUGI_HOME/takatori/third_party/fpdecimal
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-
-  cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      -DFORCE_INSTALL_RPATH=ON \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_DOCUMENTS=OFF \
-      ..
-  ninja
-  ninja install
-fi
-
-echo -e "\ntakatori started."
-cd $TSURUGI_HOME/takatori
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      -DFORCE_INSTALL_RPATH=ON \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_DOCUMENTS=OFF \
-      ..
-  ninja
-  ninja install
-fi
-
-# yugawara
-
-echo -e "\nyugawara started."
-cd $TSURUGI_HOME/yugawara
-git submodule update --init --recursive
-
-# hopscotch-map(Sub module)
-cd $TSURUGI_HOME/yugawara/third_party/hopscotch-map
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-
-  cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      -DFORCE_INSTALL_RPATH=ON \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_DOCUMENTS=OFF \
-      ..
-  #cmake --build . --target install --clean-first
-  ninja
-  ninja install
-fi
-
-cd $TSURUGI_HOME/yugawara
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      -DFORCE_INSTALL_RPATH=ON \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_DOCUMENTS=OFF \
-      ..
-  #cmake --build . --target install --clean-first
-  ninja
-  ninja install
-fi
-
-# mizugaki
-cd $TSURUGI_HOME/
-
-echo -e "\nbison started."
-if [ ! -d bison-3.5.1 ]; then
-  curl http://ftp.jaist.ac.jp/pub/GNU/bison/bison-3.5.1.tar.gz | tar zxv
-  cd bison-3.5.1
-  ./configure --prefix=$INSTALL_PREFIX
-  make -j4
-  make install
-fi
-
-echo -e "\nmizukaki started."
-cd $TSURUGI_HOME/mizugaki
-git submodule update --init --recursive
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
+  # tsurugi::sharksfin
+  'sharksfin')
+    # Build the sharksfin.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
       -DFORCE_INSTALL_RPATH=ON \
       -DBUILD_TESTS=OFF \
       -DBUILD_DOCUMENTS=OFF \
       -DBUILD_EXAMPLES=OFF \
-      ..
-  #cmake --build . --target install --clean-first
-  ninja
-  ninja install
-fi
+      -DBUILD_SHIRAKAMI=OFF
+    ;;
 
-# sandbox-performance-tools
+  # tsurugi::takatori
+  'takatori')
+    # fpdecimal (Sub module for takatori)
+    build_module 'takatori/third_party/fpdecimal'
 
-echo -e "\nsandbox-performance-tools started."
-cd $TSURUGI_HOME/sandbox-performance-tools
-git submodule update --init
+    # Build the takatori.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF
+    ;;
 
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
+  # tsurugi::decimal
+  'takatori/third_party/fpdecimal')
+    # Build the decimal.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF
+    ;;
 
-  cmake -G Ninja \
+  # tsurugi::yugawara
+  'yugawara')
+    # hopscotch-map (Sub modules for yugawara)
+    build_module 'yugawara/third_party/hopscotch-map'
+
+    # Build the yugawara.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF
+    ;;
+
+  # Tessil::hopscotch-map
+  'yugawara/third_party/hopscotch-map')
+    # Build the hopscotch-map.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF
+    ;;
+
+  # tsurugi::mizugaki
+  'mizugaki')
+    # bison (Sub modules for mizugaki)
+    build_module 'bison'
+
+    # Build the mizugaki.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF \
+      -DBUILD_EXAMPLES=OFF
+    ;;
+
+  # tsurugi::sandbox-performance-tools
+  'sandbox-performance-tools')
+    # Build the sandbox-performance-tools.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
       -DBUILD_TESTS=OFF \
       -DBUILD_DOCUMENTS=OFF \
-      -DBUILD_EXAMPLES=OFF \
-      ..
-  #cmake --build . --target install --clean-first
-  ninja
-  ninja install
-fi
+      -DBUILD_EXAMPLES=OFF
+    ;;
 
-#
-# concurrenqueue (moodycamael)
-# 
-cd $TSURUGI_HOME/jogasaki
-git submodule update --init --recursive
+  # tsurugi::jogasaki
+  'jogasaki')
+    # moodycamel::ConcurrentQueue (Sub module for jogasaki)
+    build_module 'jogasaki/third_party/concurrentqueue'
 
-echo -e "\nconcurrentqueue started."
-cd $TSURUGI_HOME/jogasaki/third_party/concurrentqueue
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
+    # tateyama (Sub modules for jogasaki)
+    build_module 'jogasaki/third_party/tateyama'
+
+    # Build the jogasaki.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-      ..
-  ninja
-  ninja install
-fi
+      -DFORCE_INSTALL_RPATH=ON \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_DOCUMENTS=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION
+    ;;
 
-#
-# tateyama
-#
-echo -e "\ntateyama started."
-cd $TSURUGI_HOME/jogasaki/third_party/tateyama
-git submodule update --init --recursive
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
+  # moodycamel::ConcurrentQueue
+  'jogasaki/third_party/concurrentqueue')
+    # Build the concurrentqueue.
+    build $module \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+    ;;
+
+  # tsurugi::tateyama
+  'jogasaki/third_party/tateyama')
+    # Build the tateyama.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
       -DBUILD_TESTS=OFF \
       -DBUILD_DOCUMENT=OFF  \
       -DBUILD_EXAMPLES=OFF \
-      -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION \
-      ..
-  ninja
-  ninja install
-fi
+      -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION
+    ;;
 
-#
-# jogasaki
-#
-echo -e "\njogasaki started."
-cd $TSURUGI_HOME/jogasaki
-
-if [ ! -d $BINARY_DIR ]; then
-  rm -rf $BINARY_DIR
-  mkdir $BINARY_DIR
-  cd $BINARY_DIR
-  cmake -G Ninja \
+  # tsurugi::ogawayama
+  'ogawayama')
+    # Build the ogawayama.
+    build $module \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+      -DCMAKE_MODULE_PATH=$INSTALL_PREFIX \
+      -DCMAKE_PREFIX_PATH=$TSURUGI_HOME/tateyama/build \
       -DFORCE_INSTALL_RPATH=ON \
       -DBUILD_TESTS=OFF \
-      -DBUILD_DOCUMENTS=OFF \
       -DBUILD_EXAMPLES=OFF \
-      -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION \
-      ..
-  #cmake --build . --target install --clean-first
-  ninja
-  ninja install
-fi
+      -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION
+    ;;
 
-# ogawayama
-echo -e "\nogawayama started."
-cd $TSURUGI_HOME/ogawayama
-git submodule update --init 
+  # GNU Bison
+  'bison')
+    if [[ ! -d $TSURUGI_HOME/bison-3.5.1 ]]; then
+      cd $TSURUGI_HOME
+      curl http://ftp.jaist.ac.jp/pub/GNU/bison/bison-3.5.1.tar.gz | tar zxv
 
-rm -rf $BINARY_DIR
-mkdir $BINARY_DIR
-cd $BINARY_DIR
-cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-    -DCMAKE_MODULE_PATH=$INSTALL_PREFIX \
-    -DCMAKE_PREFIX_PATH=$TSURUGI_HOME/tateyama/build \
-    -DFORCE_INSTALL_RPATH=ON \
-    -DBUILD_TESTS=OFF \
-    -DBUILD_EXAMPLES=OFF \
-    -DSHARKSFIN_IMPLEMENTATION=$SHARKSFIN_IMPLEMENTATION \
-    ..
-ninja
-ninja install
+      cd bison-3.5.1
+      ./configure --prefix=$INSTALL_PREFIX
+      make -j4
+      make install
+    fi
+    ;;
+  esac
 
-echo -e "\nbuild finished."
+  cd $parent_dir
+}
+
+usage_exit() {
+  echo -e "Usage:\n  $0 [options]\n" 1>&2
+  echo  "Options:" 1>&2
+  echo "  -f, --force    Forces the build to run." 1>&2
+  echo "  -c, --clean    Clean the project directory." 1>&2
+  echo "  -h, --help     Displays help." 1>&2
+  echo 1>&2
+  exit $1
+}
+
+script_main() {
+  echo -e "\nBuild script started.\n"
+
+  mkdir -p $TSURUGI_HOME
+  cd $TSURUGI_HOME
+
+  # Install the required packages.
+  package_install
+
+  # Get the source file.
+  get_source_file
+
+  # Build the components.
+  build_module
+
+  echo -e "\nBuild script finished."
+}
+
+script_main
