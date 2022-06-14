f21eef8777294996a56790c2ee9249ace7348ed9
Yusuke Kitajima <yus-kitajima@nec.com>
2022-06-15 08:59:05 +0900

    Added BEGIN/END DDL MessageId and Initialize function.

--- include/manager/message/message.h
+++ include/manager/message/message.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 tsurugi project.
+ * Copyright 2020-2022 tsurugi project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,95 +25,109 @@
 
 #include "manager/message/receiver.h"
 
-namespace manager::message
-{
-    class Receiver;
+namespace manager::message {
+class Receiver;
 
-    const std::string MESSAGE_TYPE_NANE_CREATE_TABLE = "CREATE TABLE";
+const std::string MESSAGE_TYPE_NANE_CREATE_TABLE = "CREATE TABLE";
+const std::string MESSAGE_TYPE_NANE_BEGIN_DDL = "BEGIN DDL";
+const std::string MESSAGE_TYPE_NANE_END_DDL = "END DDL";
 
-    /**
-     * @enum MessageId
-     * @brief message type ID that is an uniquely determined statement ID inputted by user.
-     */
-    enum class MessageId : int
-    {
-        /**
-         *  @brief CREATE TABLE statement.
-         */
-        CREATE_TABLE = 0
-    };
+/**
+ * @enum MessageId
+ * @brief message type ID that is an uniquely determined statement ID inputted by user.
+ */
+enum class MessageId : int {
+  /**
+   *  @brief CREATE TABLE statement.
+   */
+  CREATE_TABLE = 0,
+  BEGIN_DDL,         //!< @brief BEGIN DDL statement.
+  END_DDL            //!< @brief END DDL statement.
+};
 
-    class Message {
-        public:
-            /**
-             * @brief C'tor. Initialize member variables.
-             * @param [in] id message type ID that is an uniquely determined statement ID inputted by user.
-             * @param [in] object_id object ID that will be added, updated, or deleted.
-             * @param [in] message_type_name string in order to report error messages.
-             */
-            Message(MessageId id, uint64_t object_id, std::string message_type_name)
-                : id(id), object_id(object_id), message_type_name(message_type_name){};
+class Message {
+ public:
+  /**
+   * @brief C'tor. Initialize member variables.
+   * @param [in] id message type ID that is an uniquely determined statement ID inputted by user.
+   * @param [in] object_id object ID that will be added, updated, or deleted.
+   * @param [in] message_type_name string in order to report error messages.
+   */
+  Message(MessageId id, uint64_t object_id, std::string message_type_name)
+      : id(id), object_id(object_id), message_type_name(message_type_name){};
 
-            /**
-             * @brief Set a receiver that will receive messages.
-             * @param [in] receiver_ a receiver
-             */
-            void set_receiver(Receiver *receiver_) {
-                receivers.push_back(receiver_);
-            };
+  /**
+   * @brief Set a receiver that will receive messages.
+   * @param [in] receiver_ a receiver
+   */
+  void set_receiver(Receiver* receiver_) { receivers.push_back(receiver_); };
 
-            /**
-             * @brief Get message type ID that is an uniquely determined statement ID inputted by user.
-             * @return message type ID.
-             */
-            MessageId get_id(){
-                return id;
-            };
+  /**
+   * @brief Get message type ID that is an uniquely determined statement ID
+   * inputted by user.
+   * @return message type ID.
+   */
+  MessageId get_id() { return id; };
 
-            /**
-             * @brief Get object ID that will be added, updated, or deleted.
-             * @return object ID.
-             */
-            uint64_t get_object_id(){
-                return object_id;
-            };
+  /**
+   * @brief Get object ID that will be added, updated, or deleted.
+   * @return object ID.
+   */
+  uint64_t get_object_id() { return object_id; };
 
-            /**
-             * @brief Get all receivers that will receive messages.
-             * @return all receivers.
-             */
-            std::vector<Receiver *> get_receivers()
-            {
-                return receivers;
-            };
+  /**
+   * @brief Get all receivers that will receive messages.
+   * @return all receivers.
+   */
+  std::vector<Receiver*> get_receivers() { return receivers; };
 
-            /**
-             * @brief Get message type name to report error messages.
-             * @return message type name.
-             */
-            std::string get_message_type_name()
-            {
-                return message_type_name;
-            };
+  /**
+   * @brief Get message type name to report error messages.
+   * @return message type name.
+   */
+  std::string get_message_type_name() { return message_type_name; };
 
-        private:
-            MessageId id; //!< @brief message type ID that is an uniquely determined statement ID inputted by user.
-            uint64_t object_id; //!< @brief object ID that will be added, updated, or deleted.
-            std::vector<Receiver *> receivers; //!< @brief receivers that will receive message ex) OLTP_Receiver, OLAP_Receiver.
-            std::string message_type_name; //!< @brief string in order to report error messages ex)"CREATE TABLE"
-    };
+ private:
+  MessageId id;                     //!< @brief message type ID that is an uniquely determined statement ID inputted by user.
+  uint64_t object_id;               //!< @brief object ID that will be added, updated, or deleted.
+  std::vector<Receiver*> receivers; //!< @brief receivers that will receive message ex) OLTP_Receiver, OLAP_Receiver.
+  std::string message_type_name;    //!< @brief string in order to report error messages ex)"CREATE TABLE"
+};
 
-    class CreateTableMessage : public Message
-    {
-        public:
-            /**
-             * @brief C'tor. Initialize member variables.
-             * @param [in] object_id object ID that will be added, updated, or deleted.
-             */
-            CreateTableMessage(uint64_t object_id)
-                : Message{MessageId::CREATE_TABLE, object_id, MESSAGE_TYPE_NANE_CREATE_TABLE} {}
-    };
+class CreateTableMessage : public Message {
+ public:
+  /**
+   * @brief C'tor. Initialize member variables.
+   * @param [in] object_id object ID that will be added, updated, or deleted.
+   */
+  CreateTableMessage(uint64_t object_id)
+      : Message{MessageId::CREATE_TABLE, object_id,
+                MESSAGE_TYPE_NANE_CREATE_TABLE} {}
+};
 
-}; // namespace manager::message
+class BeginDDLMessage : public Message {
+ public:
+  /**
+   * @brief C'tor. Initialize member variables.
+   * @param [in] object_id object ID that will be added, updated, or deleted.
+   */
+  BeginDDLMessage(uint64_t object_id)
+      : Message{MessageId::BEGIN_DDL, object_id,
+                MESSAGE_TYPE_NANE_BEGIN_DDL} {}
+};
 
-#endif // MESSAGE_H
+class EndDDLMessage : public Message {
+ public:
+  /**
+   * @brief C'tor. Initialize member variables.
+   * @param [in] object_id object ID that will be added, updated, or deleted.
+   */
+  EndDDLMessage(uint64_t object_id)
+      : Message{MessageId::END_DDL, object_id,
+                MESSAGE_TYPE_NANE_END_DDL} {}
+};
+
+
+};  // namespace manager::message
+
+#endif  // MESSAGE_H
