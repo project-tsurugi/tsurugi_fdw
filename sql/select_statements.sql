4e47162f4955cfd806d267e1e335ae6e3e32dbd0
Kohei Okada <k-okada-pb@nec.com>
2023-02-07 09:53:33 +0900

    Update SELECT tests.
    Signed-off-by: Kohei Okada <k-okada-pb@nec.com>

--- sql/select_statements.sql
+++ sql/select_statements.sql
@@ -1,4 +1,5 @@
 /* DDL */
+-- TG tables
 CREATE TABLE t1(
     c1 INTEGER PRIMARY KEY, 
     c2 INTEGER, 
@@ -39,6 +40,7 @@ CREATE FOREIGN TABLE t2(
     c7 VARCHAR(26)
 ) SERVER ogawayama;
 
+-- PG tables
 CREATE TABLE pt1(
     c1 INTEGER PRIMARY KEY, 
     c2 INTEGER, 
@@ -60,9 +62,7 @@ CREATE TABLE pt2(
 );
 
 /* DML */
-SELECT * FROM t1;
-SELECT c2, c4, c6 FROM t2;
-
+-- TG tables
 INSERT INTO 
     t1(c1, c2, c3, c4, c5, c6, c7)
 VALUES
@@ -76,20 +76,19 @@ INSERT INTO
 VALUES
     (3, 33, 333, 3.3, 3.33, 'third', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 SELECT * FROM t1;
-SELECT c1, c3, c5, c7 FROM t1 WHERE c1 = 2;
 
 INSERT INTO 
     t2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
-    (1, 11, 111, 1.1, 1.11, 'one', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (1, 11, 111, 1.1, 1.11, 'one', 'ABC');
 INSERT INTO 
     t2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
-    (2, 22, 222, 2.2, 2.22, 'two', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (2, 22, 222, 2.2, 2.22, 'two', 'XYZ');
 INSERT INTO 
     t2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
-    (3, 33, 333, 3.3, 3.33, 'three', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (3, 33, 333, 3.3, 3.33, 'three', 'ABC');
 INSERT INTO 
     t2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
@@ -97,10 +96,10 @@ VALUES
 INSERT INTO 
     t2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
-    (5, 55, 555, 5.5, 5.55, 'five', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (5, 55, 555, 5.5, 5.55, 'five', 'XYZ');
 SELECT * FROM t2;
-SELECT c6 FROM t2;
 
+-- PG tables
 INSERT INTO 
     pt1 
 VALUES
@@ -118,15 +117,15 @@ SELECT * FROM pt1;
 INSERT INTO 
     pt2 
 VALUES
-    (1, 11, 111, 1.1, 1.11, 'one', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (1, 11, 111, 1.1, 1.11, 'one', 'ABC');
 INSERT INTO 
     pt2 
 VALUES
-    (2, 22, 222, 2.2, 2.22, 'two', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (2, 22, 222, 2.2, 2.22, 'two', 'XYZ');
 INSERT INTO 
     pt2 
 VALUES
-    (3, 33, 333, 3.3, 3.33, 'three', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (3, 33, 333, 3.3, 3.33, 'three', 'ABC');
 INSERT INTO 
     pt2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
@@ -134,10 +133,290 @@ VALUES
 INSERT INTO 
     pt2(c1, c2, c3, c4, c5, c6, c7)
 VALUES
-    (5, 55, 555, 5.5, 5.55, 'five', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    (5, 55, 555, 5.5, 5.55, 'five', 'XYZ');
 SELECT * FROM pt2;
 
+-- SELECT
+-- PG
+SELECT * FROM pt1;
+-- TG
+SELECT * FROM t1;
+
+-- PG
+SELECT c2, c4, c6 FROM pt2;
+-- TG
+SELECT c2, c4, c6 FROM t2;
+
+-- PG
+SELECT c6 FROM pt2;
+-- TG
+SELECT c6 FROM t2;
+
+-- PG
+SELECT c1, c2 AS seisu FROM pt2;
+-- TG
+SELECT c1, c2 AS seisu  FROM t2;
+
+-- ORDER BY #1
+-- PG
+SELECT
+    *
+FROM
+    pt2
+ORDER BY 
+    c6;
+
+-- TG
+SELECT
+    *
+FROM
+    t2
+ORDER BY 
+    c6;
+
+-- ORDER BY #2
+-- PG
+SELECT
+    c1, c2, c3, c4, c5, c6, c7
+FROM
+    pt2
+ORDER BY 
+    6;
+
+-- TG
+SELECT
+    c1, c2, c3, c4, c5, c6, c7
+FROM
+    t2
+ORDER BY 
+    6;
+
+-- ORDER BY #3
+-- PG
+SELECT
+    c1, c4, c5, c6, c7
+FROM
+    pt2
+WHERE
+    c1 > 2
+ORDER BY
+    c7;
+
+-- TG
+SELECT
+    c1, c4, c5, c6, c7
+FROM
+    t2
+WHERE
+    c1 > 2
+ORDER BY
+    c7;
+
+-- ORDER BY #4
+-- PG
+SELECT
+    c1, c2
+FROM
+    pt2
+WHERE
+    c2 * 2 > 50
+ORDER BY
+    c3 DESC;
+
+-- TG
+SELECT
+    c1, c2
+FROM
+    t2
+WHERE
+    c2 * 2 > 50
+ORDER BY
+    c3 DESC;
+
+-- WHERE #1
+-- PG
+SELECT
+    *
+FROM
+    pt2
+WHERE
+    c3 > 2
+ORDER BY
+    c1;
+
+-- TG
+SELECT
+    *
+FROM
+    t2
+WHERE
+    c3 > 2
+ORDER BY
+    c1;
+
+-- WHERE #2
+-- PG
+SELECT
+    *
+FROM
+    pt2
+WHERE
+    c7 = 'XYZ'
+ORDER BY
+    c1;
+
+-- TG
+SELECT
+    *
+FROM
+    t2
+WHERE
+    c7 = 'XYZ'
+ORDER BY
+    c1;
+
+-- WHERE #3
+-- PG
+SELECT 
+    c1, c2, c6, c7
+FROM
+    pt1
+WHERE
+    c1 = 1 OR c1 = 3;
+
+-- TG
+SELECT 
+    c1, c2, c6, c7
+FROM
+    t1
+WHERE
+    c1 = 1 OR c1 = 3;
+
+-- WHERE #4
+-- PG
+SELECT 
+    c1, c3, c5, c7 
+FROM 
+    pt1 
+WHERE 
+    c1 = 2;
+
+-- TG
+SELECT 
+    c1, c3, c5, c7 
+FROM 
+    t1 
+WHERE 
+    c1 = 2;
+
+-- WHERE #5
+-- PG
+SELECT
+    *
+FROM
+    pt1
+WHERE
+    c4 >= 2.2 AND c4 < 5.5;
+
+-- TG
+SELECT
+    *
+FROM
+    t1
+WHERE
+    c4 >= 2.2 AND c4 < 5.5;
+
+-- WHERE #6
+-- PG
+SELECT
+    *
+FROM
+    pt2
+WHERE
+    c4 BETWEEN 2.2 AND 5.5;
+
+-- TG
+SELECT
+    *
+FROM
+    t2
+WHERE
+    c4 BETWEEN 2.2 AND 5.5;
+
+-- WHERE #7
+-- PG
+SELECT
+    *
+FROM
+    pt1
+WHERE
+    c7 LIKE '%LMN%';
+
+-- TG
+SELECT
+    *
+FROM
+    t1
+WHERE
+    c7 LIKE '%LMN%';
+
+-- GROUP BY #1
+-- PG
+SELECT
+    count(c1) AS "count(c1)", sum(c2) AS "sum(c2)", c7
+FROM 
+    pt2
+GROUP BY
+    c7;
+
+-- TG
+SELECT
+    count(c1) AS "count(c1)", sum(c2) AS "sum(c2)", c7
+FROM 
+    t2
+GROUP BY
+    c7;
+
+-- GROUP BY #2
+-- PG
+SELECT
+    count(c1) AS "count(c1)", sum(c2) AS "sum(c2)", c7
+FROM 
+    pt2
+GROUP BY
+    c7
+HAVING 
+    sum(c2) > 55;
+
+-- TG
+SELECT
+    count(c1) AS "count(c1)", sum(c2) AS "sum(c2)", c7
+FROM 
+    t2
+GROUP BY
+    c7
+HAVING 
+    sum(c2) > 55;
+
+-- GROUP BY #3
+-- PG
+SELECT
+    c7, count(c1), sum(c2), avg(c3), min(c4), max(c5)
+FROM
+    pt2
+GROUP BY
+    c7;
+
+-- TG
+SELECT
+    c7, count(c1), sum(c2), avg(c3), min(c4), max(c5)
+FROM
+    pt2
+GROUP BY
+    c7;
+
 -- FOREIGN TABLE JOIN
+-- TG JOIN #1
 SELECT 
     *
 FROM 
@@ -145,27 +424,31 @@ FROM
 WHERE
     b.c1 > 1;
 
+-- TG JOIN #2
 SELECT 
-    a.c1, b.c2, a.c4, b.c6
+    a.c1, a.c2, b.c1, b.c7
 FROM 
     t1 a INNER JOIN t2 b USING (c1)
 ORDER BY
     b.c4 DESC;   
 
+-- TG JOIN #3
 SELECT 
-    a.c1, b.c3, a.c5, b.c7
+    a.c1, a.c3, b.c1, b.c6
 FROM 
     t1 a INNER JOIN t2 b ON a.c1 != b.c1;
 
 -- POSTGRESQL TABLE JOIN
--- #1
-SELECT 
+-- PG JOIN #1
+-- PG
+SELECT
     *
 FROM 
     pt1 a INNER JOIN pt2 b ON a.c4 = b.c4
 WHERE
     a.c1 > 1;
 
+-- TG
 SELECT 
     *
 FROM 
@@ -173,7 +456,8 @@ FROM
 WHERE
     a.c1 > 1;
 
--- #2
+-- PG JOIN #2
+-- PG
 SELECT 
     a.c1, a.c2, b.c4, b.c6
 FROM 
@@ -181,6 +465,7 @@ FROM
 ORDER BY
     b.c4 DESC;
 
+-- TG
 SELECT 
     a.c1, a.c2, b.c4, b.c6
 FROM 
@@ -188,18 +473,21 @@ FROM
 ORDER BY
     b.c4 DESC;
 
--- #3
+-- PG JOIN  #3
+-- PG
 SELECT 
     a.c1, a.c3, b.c5, b.c7
 FROM 
     pt2 a LEFT OUTER JOIN pt1 b ON a.c1 = b.c1;
 
+-- TG
 SELECT 
     a.c1, a.c3, b.c5, b.c7
 FROM 
-    t2 a LEFT OUTER JOIN pt2 b ON a.c1 = b.c1;
+    t2 a LEFT OUTER JOIN pt1 b ON a.c1 = b.c1;
 
--- #4
+-- PG JOIN #4
+-- PG
 SELECT 
     *
 FROM 
@@ -207,6 +495,7 @@ FROM
 WHERE
     a.c1 > 1 AND a.c1 < 3;
 
+-- TG
 SELECT 
     *
 FROM 
@@ -214,7 +503,8 @@ FROM
 WHERE
     a.c1 > 1 AND a.c1 < 3;
 
--- #5
+-- PG JOIN #5
+-- PG
 SELECT
     count(a.c1), sum(b.c2), b.c7
 FROM 
@@ -222,6 +512,7 @@ FROM
 GROUP BY
     b.c7;
 
+-- TG
 SELECT
     count(a.c1), sum(b.c2), b.c7
 FROM 
