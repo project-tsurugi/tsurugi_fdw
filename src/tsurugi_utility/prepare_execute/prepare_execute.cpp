d77f3c1f399be23eff0ee31c1abc93f5c584828a
fujita-kin <fujita-kin@nec.com>
2024-10-23 11:28:49 +0900

    Remove table checks for PREPARE and EXECUTE commands for the transaction specification feature.

--- src/tsurugi_utility/prepare_execute/prepare_execute.cpp
+++ src/tsurugi_utility/prepare_execute/prepare_execute.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Project Tsurugi.
+ * Copyright 2023-2024 Project Tsurugi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,87 +100,6 @@ get_tsurugi_table_join_expr(JoinExpr* join,
 		RangeVar* relation = (RangeVar *) join->rarg;
 		target_tables.emplace_back(relation->relname);
 	}
-}
-
-/**
- *  @brief Tsurugi OLTP table exist in the target relation.
- *  @param [in] query
- *  @return true if Tsurugi OLTP table exists.
- */
-bool
-is_tsurugi_table(Node* query,
-				 bool show_warning)
-{
-	bool result = false;
-	bool is_tsurugi = false;
-	bool is_not_tsurugi = false;
-	std::vector<std::string> target_tables;
-
-	switch (nodeTag(query))
-	{
-		case T_InsertStmt:
-			{
-				InsertStmt* stmt = (InsertStmt *) query;
-				target_tables.emplace_back(stmt->relation->relname);
-			}
-			break;
-		case T_UpdateStmt:
-			{
-				UpdateStmt* stmt = (UpdateStmt *) query;
-				target_tables.emplace_back(stmt->relation->relname);
-			}
-			break;
-		case T_DeleteStmt:
-			{
-				DeleteStmt* stmt = (DeleteStmt *) query;
-				target_tables.emplace_back(stmt->relation->relname);
-			}
-			break;
-		case T_SelectStmt:
-			{
-				SelectStmt* stmt = (SelectStmt *) query;
-				ListCell* l;
-				foreach(l, stmt->fromClause)
-				{
-					Node* from = (Node *) lfirst(l);
-					if (IsA(from, RangeVar)) {
-						RangeVar* relation = (RangeVar *) from;
-						target_tables.emplace_back(relation->relname);
-					}
-					if (IsA(from, JoinExpr)) {
-						get_tsurugi_table_join_expr((JoinExpr *) from, target_tables);
-					}
-				}
-			}
-			break;
-		default:
-			/* should not reach here */
-			elog(ERROR, "is_tsurugi_table: unrecognized node type: %d",
-				 (int) nodeTag(query));
-			return result;
-	}
-
-	auto tables = manager::metadata::get_tables_ptr(TSURUGI_DB_NAME);
-	for(std::size_t i = 0; i < target_tables.size(); i++) {
-		if (tables->exists(target_tables[i])) {
-			is_tsurugi = true;
-		} else {
-			is_not_tsurugi = true;
-		}
-	}
-
-	if (is_tsurugi) {
-		if (is_not_tsurugi) {
-			if (show_warning) {
-				elog(WARNING,
-					"If Tsurugi and non-Tsurugi tables are mixed, do not PREPARE to Tsurugi");
-			}
-		} else {
-			result = true;
-		}
-	}
-
-	return result;
 }
 
 /**
@@ -1244,10 +1163,6 @@ after_prepare_stmt(const PrepareStmt* stmts,
 	Assert(stmts != nullptr);
 
 	Node* query = stmts->query;
-	if (!is_tsurugi_table(query, true)) {
-		return true;
-	}
-
 	stub::placeholders_type placeholders{};
 	StringInfoData sql;
 	initStringInfo(&sql);
@@ -2210,12 +2125,7 @@ befor_execute_stmt(const ExecuteStmt* stmts,
 	ListCell* l;
 
 	entry = FetchPreparedStatement(stmts->name, true);
-
 	RawStmt* raw_stmt = entry->plansource->raw_parse_tree;
-	Node* query = raw_stmt->stmt;
-	if (!is_tsurugi_table(query, false)) {
-		return true;
-	}
 
 	analyze_execut_parameters(stmts, entry, queryString);
 
@@ -2283,13 +2193,6 @@ befor_execute_stmt(const ExecuteStmt* stmts,
 bool
 after_execute_stmt(const ExecuteStmt* stmts)
 {
-	PreparedStatement* entry = FetchPreparedStatement(stmts->name, true);
-	RawStmt* raw_stmt = entry->plansource->raw_parse_tree;
-	Node* query = raw_stmt->stmt;
-	if (!is_tsurugi_table(query, false)) {
-		return true;
-	}
-
 	stored_prepare_statment[stmts->name] = std::move(prepared_statement);
 	stmts_name = {};
 	parameters = {};
