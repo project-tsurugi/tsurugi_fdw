4995bfd3a888ebdfc90d927b104faf2da16a56c1
oniki <oniki.naokazu@kcc.co.jp>
2025-08-29 15:27:27 +0900

    Added functionality to set the Tsurugi database name. Support for '{CREATE|ALTER} SERVER ... OPTIONS (...)'

--- tsurugi_fdw--1.2.0--1.3.0.sql
+++ tsurugi_fdw--1.2.0--1.3.0.sql
@@ -1,3 +1,10 @@
+CREATE OR REPLACE FUNCTION tsurugi_fdw_validator(text[], oid)
+  RETURNS void AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+ALTER FOREIGN DATA WRAPPER tsurugi_fdw
+  VALIDATOR tsurugi_fdw_validator;
+
 CREATE OR REPLACE FUNCTION tg_show_tables
   (text DEFAULT null, text DEFAULT null, text DEFAULT 'detail', boolean DEFAULT true)
   RETURNS JSON AS 'tsurugi_fdw' LANGUAGE C;
