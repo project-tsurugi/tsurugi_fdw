71eea4e8c4c1871797f66bd2bb63067507597547
oniki <oniki.naokazu@kcc.co.jp>
2025-10-27 16:20:50 +0900

    Modified the basic test.

--- sql/case_sensitive_happy.sql
+++ sql/case_sensitive_happy.sql
@@ -18,7 +18,7 @@ CREATE FOREIGN TABLE fdw_case_table (
 INSERT INTO fdw_case_table VALUES (1000, 1);
 INSERT INTO public.fdw_case_table (col, "Col") VALUES (2000, 2);
 INSERT INTO "public".fdw_case_table (col) VALUES (3000);
-INSERT INTO "public"."fdw_case_table" (Col) VALUES (4);
+INSERT INTO "public"."fdw_case_table" (Col) VALUES (4000);
 INSERT INTO Public.fdw_case_table ("col") VALUES (5000);
 INSERT INTO PUBLIC.fdw_case_table ("Col") VALUES (6);
 SELECT * FROM public.fdw_case_table ORDER BY col, Col DESC;
@@ -26,20 +26,21 @@ SELECT * FROM public.fdw_case_table ORDER BY col, Col DESC;
 UPDATE fdw_case_table SET col = col + 100;
 UPDATE public.fdw_case_table SET Col = Col + 10;
 UPDATE "public".fdw_case_table SET "col" = "col" + 100 WHERE col > 5000;
-UPDATE public."fdw_case_table" SET "Col" = "Col" + 100 WHERE Col > 12;
+UPDATE public."fdw_case_table" SET "Col" = "Col" + 10 WHERE "Col" < 10;
 UPDATE "public"."fdw_case_table" SET "col" = col + 100 WHERE "col" > 3000;
-UPDATE Public.fdw_case_table SET "col" = Col * 10 WHERE col IS NULL;
-UPDATE PUBLIC.fdw_case_table SET "Col" = col / 100 WHERE "Col" IS NULL;
+UPDATE public.Fdw_Case_Table SET "col" = NULL WHERE "Col" = 16;
+UPDATE Public.fdw_case_table SET "col" = "Col" * 10 WHERE Col IS NULL;
+UPDATE PUBLIC.fdw_case_table SET "Col" = Col / 100 WHERE "Col" IS NULL;
 SELECT * FROM fdw_case_table ORDER BY col;
-SELECT * FROM Public.fdw_case_table ORDER BY Col;
+SELECT * FROM Public.Fdw_Case_Table ORDER BY Col;
 
-DELETE FROM fdw_case_table WHERE col = 1100;
+DELETE FROM fdw_case_table WHERE col = 1110;
 DELETE FROM public.fdw_case_table WHERE "col" > 5000;
-DELETE FROM "public".fdw_case_table WHERE Col > 114;
-DELETE FROM public."fdw_case_table" WHERE "col" >= 3200;
-DELETE FROM "public"."fdw_case_table" WHERE "Col" > 100;
-DELETE FROM Public.fdw_case_table WHERE col <> 2100;
-DELETE FROM PUBLIC.fdw_case_table WHERE Col <> 12;
+DELETE FROM "public".fdw_case_table WHERE Col <= 160;
+DELETE FROM public."fdw_case_table" WHERE "col" >= 4200;
+DELETE FROM "public"."fdw_case_table" WHERE "Col" > 30;
+DELETE FROM Public.fdw_case_table WHERE col <> 2110;
+DELETE FROM PUBLIC.fdw_case_table WHERE Col = 12;
 SELECT * FROM Public.fdw_case_table;
 
 -- Test teardown: DDL of the PostgreSQL
