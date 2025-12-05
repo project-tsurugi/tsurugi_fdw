40b893c62d17c0b8fd19b2ba1e886d81ddbd38ed
koh-okada <k-okada-pb@nec.com>
2025-12-05 16:17:17 +0900

    Remove tsurugi_error() and get_error_detail().

--- include/common/tsurugi.h
+++ include/common/tsurugi.h
@@ -213,8 +213,6 @@ public:
 	ERROR_CODE get_table_metadata(Oid server_oid, std::string_view table_name,
 										 TableMetadataPtr& table_metadata);
 
-    ERROR_CODE tsurugi_error(ogawayama::stub::tsurugi_error_code& code);
-    std::string get_error_detail(ERROR_CODE error_code);
     std::string get_error_message(ERROR_CODE error_code);
     void error_log2(int level, std::string_view message, ERROR_CODE error);
     void error_log3(int level, std::string_view message, ERROR_CODE error);
